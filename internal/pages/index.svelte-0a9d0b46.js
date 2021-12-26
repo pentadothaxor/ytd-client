b71a472d3eb21628d0581f5c0e5ba2f6dfdff76d
pentadothaxor <90568095+pentadothaxor@users.noreply.github.com>
2021-12-26 17:36:27 +0200

    Add files via upload

---
+++ internal/pages/index.svelte-0a9d0b46.js
@@ -0,0 +1,1 @@
+import{S as t,i as s,s as o,A as a}from"../chunks/vendor-e593b835.js";function l(e){return a(async()=>{window.location.replace("/en/"),document.querySelector("html").lang=lang}),[]}class u extends t{constructor(n){super();s(this,n,l,null,o,{})}}export{u as default};
