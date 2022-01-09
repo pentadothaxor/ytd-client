ee25294618a1b1e6dcbd7b88203606774c57441d
pentadothaxor <90568095+pentadothaxor@users.noreply.github.com>
2022-01-09 16:35:50 +0200

    a

--- internal/pages/index.svelte-73e042e5.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as s,s as o,A as a}from"../chunks/vendor-e593b835.js";function l(e){return a(async()=>{window.location.replace("/en"),document.querySelector("html").lang=lang}),[]}class u extends t{constructor(n){super();s(this,n,l,null,o,{})}}export{u as default};
